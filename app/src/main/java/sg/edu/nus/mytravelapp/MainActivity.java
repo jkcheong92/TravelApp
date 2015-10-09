9193479b11383235ecbab81cad727cb972231552
Jack <jack@shopback.com>
2015-10-09 14:31:21 +0800

    Added intent to map from main

--- app/src/main/java/sg/edu/nus/mytravelapp/MainActivity.java
+++ app/src/main/java/sg/edu/nus/mytravelapp/MainActivity.java
@@ -157,34 +157,9 @@ public class MainActivity extends Activity {
         startActivity(i);
     }
 
-    // TODO: Put this in Map Activity
-    public void checkBattery() {
-        IntentFilter intentFilter = new IntentFilter(Intent.ACTION_BATTERY_CHANGED);
-
-        Intent batteryStatus = this.registerReceiver(null, intentFilter);
-
-        // Is battery charging or full?
-        int status = batteryStatus.getIntExtra(BatteryManager.EXTRA_STATUS, -1);
-        boolean isCharging = status == BatteryManager.BATTERY_STATUS_CHARGING || status == BatteryManager.BATTERY_STATUS_FULL;
-
-        // How are we charging the battery?
-        int chargePlug = batteryStatus.getIntExtra(BatteryManager.EXTRA_PLUGGED, -1);
-        boolean usbCharge = chargePlug == BatteryManager.BATTERY_PLUGGED_USB;
-        boolean acCharge = chargePlug == BatteryManager.BATTERY_PLUGGED_AC;
-
-        // Check battery level
-        int level = batteryStatus.getIntExtra(BatteryManager.EXTRA_LEVEL, -1);
-        int scale = batteryStatus.getIntExtra(BatteryManager.EXTRA_SCALE, -1);
-        float batteryPercent = level/(float)scale * 100;
-
-        // Toast message if battery level is low and user is not charging phone
-        if (batteryPercent < 50 && isCharging == false)
-            Toast.makeText(MainActivity.this, "Your device battery level is low now: " + batteryPercent + "%! Are you sure you want to continue?", Toast.LENGTH_SHORT).show();
-
-        if (usbCharge)
-            Toast.makeText(MainActivity.this, "Charging phone by usb", Toast.LENGTH_SHORT).show();
-        if (acCharge)
-            Toast.makeText(MainActivity.this, "Charging phone by acCharge", Toast.LENGTH_SHORT).show();
+    public void onClick_map(View view) {
+        Intent i = new Intent(this, Map.class);
+        startActivity(i);
     }
 
 }
